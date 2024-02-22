856c683dd7ad548abcdb67b0c7240f71e7ed8358
MinNguTrue <minngutru@gmail.com>
2024-02-21 22:02:12 -0800

    transaction.h
    Minh Nguyen

---
+++ transaction.h
@@ -0,0 +1,23 @@
+#include "pateron.h"
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+#include <iostream>
+using namespace std;
+
+class Transaction {
+public:
+    Transaction();
+    Transaction(Pateron* pateron, Book* book);
+    ~Transaction();
+    bool returnBook();
+    bool checkOutBook();
+    void bookInfo();
+    void transactionInfo();
+
+private:
+    Patreon* patreon;
+    Book* book;
+    bool checkOut; // True if order is checked out, false if returned
+};
+
+#endif
